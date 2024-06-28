4649c18401a3f1e28cfdff1c7ad85ce2cdff5c1e
Kenneth Voltaire Narvaez <nkennethvoltaire@gmail.com>
2024-06-28 18:43:59 +0800

    Creating Index.php
    Making a Form for Upload File

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSV Import to MySQL</title>
+</head>
+<body style="text-align: center;">
+    <h2>CSV Import to MySQL</h2>
+
+    <form method="post" enctype="multipart/form-data">
+        Select CSV file to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload CSV" name="submit">
+    </form>
+    
+</body>
+</html>
